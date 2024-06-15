e1b6fbfdd672722b8d1ad67f2a18cc7c5be5a735
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-15 10:01:19 +0400

    added test for dependency injector

---
+++ tests/SimpleClasses/SimpleDefinition.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Henrik\DI\Test\SimpleClasses;
+
+class SimpleDefinition
+{
+    /**
+     * @param string[] $cookies
+     */
+    public function __construct(private array $cookies = []) {}
+
+    /**
+     * @return string[]
+     */
+    public function getCookies(): array
+    {
+        return $this->cookies;
+    }
+
+    /**
+     * @param string[] $cookies
+     *
+     * @return void
+     */
+    public function setCookies(array $cookies): void
+    {
+        $this->cookies = $cookies;
+    }
+}
